0e53d3885c49b451304b0692f3aca0b9ccd5a7b1
Viki Oberth <91355704+Vixcalibur@users.noreply.github.com>
2025-09-02 11:26:33 -0400

    Update row data for listing in main.py: Fixed Coins being Bullion

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def generate_csv(
                 price=f"{prices[i]}"
                 listingName = j_customLabelSKU + " C: LIVE " + title + " RTTV"
                 desc = f"Item Shown on Screen During {fullTitle}"
-                row = ["Add", j_customLabelSKU, "39489", "/Coins & Paper Money/Bullion/Silver/Bars & Rounds",
+                row = ["Add", j_customLabelSKU, "525", "/Coins & Paper Money/Coins: US/Collections, Lots",
                        listingName, "", "", "", "", price, "1", photoURL, "", "3000-Used", desc, "Auction", "7", "", "", "", "",
                        "", "Marietta, GA", "", "", "", "", "", "", "", "", "", "", "", shipping,
                        "No Return Accepted (234360674026) - (ID: 234360674026)",
